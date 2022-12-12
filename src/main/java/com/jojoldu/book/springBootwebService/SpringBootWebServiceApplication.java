0b7417d50a5b34d99f5ff547d8d02bca80de3b6f
kyunyan <psog03081@naver.com>
2022-12-12 23:52:30 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/springBootwebService/SpringBootWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springBootwebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/springBootwebService/SpringBootWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springBootwebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
